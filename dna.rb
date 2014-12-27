89b10a4071e15e4830451a8d61a7e72c46b34acc
Denis Patin <denpatin@gmail.com>
2014-12-28 01:17:38 +0300

    SOLVED the dna task

---
+++ dna.rb
@@ -0,0 +1,16 @@
+# USAGE:
+# 
+# Input:
+# $ ruby dna.rb /path/to/the/file-with-DNA-string
+# 
+# Output:
+# Four integers separated by spaces counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in the given DNA string.
+
+# Declaring the resulting hash
+counts = Hash.new(0)
+
+# Reading molecule names from the file and counting their occurrences
+File.open(ARGV[0]).read.split('').each { |molecule| counts[molecule] += 1 if %w(A C G T).include? molecule }
+
+# Outputting the occurrences in the requested order
+puts counts.sort.each { |count| counts[count] }.to_h.values.join(' ')
