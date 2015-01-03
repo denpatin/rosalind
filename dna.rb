b19e439ea974f717522ac50d2534838d9f83a9cf
Denis Patin <denpatin@gmail.com>
2015-01-03 14:26:19 +0300

    Fixed style errors

--- dna.rb
+++ dna.rb
@@ -1,16 +1,19 @@
 # USAGE:
-# 
+#
 # Input:
 # $ ruby dna.rb /path/to/the/file-with-DNA-string
-# 
+#
 # Output:
-# Four integers separated by spaces counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in the given DNA string.
+# Four integers separated by spaces counting the respective number of times
+# that the symbols 'A', 'C', 'G', and 'T' occur in the given DNA string.
 
 # Declaring the resulting hash
 counts = Hash.new(0)
 
 # Reading molecule names from the file and counting their occurrences
-File.open(ARGV[0]).read.split('').each { |molecule| counts[molecule] += 1 if %w(A C G T).include? molecule }
+File.open(ARGV[0]).read.split('').each do |molecule|
+  counts[molecule] += 1 if %w(A C G T).include? molecule
+end
 
 # Outputting the occurrences in the requested order
 puts counts.sort.each { |count| counts[count] }.to_h.values.join(' ')
--- rna.rb
+++ rna.rb
@@ -1,10 +1,10 @@
 # USAGE:
-# 
+#
 # Input:
 # $ ruby rna.rb /path/to/the/file-with-DNA-string
-# 
+#
 # Output:
 # The transcribed RNA string.
 
-# Reading the DNA string in the file, transcribing it, and outputting the RNA string
+# Reading the DNA string, transcribing it, and outputting the RNA string
 puts File.open(ARGV[0]).read.gsub(/T/, 'U')
